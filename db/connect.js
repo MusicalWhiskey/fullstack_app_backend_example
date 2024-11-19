e7f7556252cbe56513d47d34c2bebaa35808c7a9
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-11-19 09:43:09 -0700

    updated atlas_uri

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.ATLAS_URI);
         console.log(`Connected to MongoDB ${conn.connections[0].name}`);
         
     } catch (e) {
